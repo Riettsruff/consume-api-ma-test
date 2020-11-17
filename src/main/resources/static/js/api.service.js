23bf102239d6bff28d8e6b23f5a538e64cf15522
Rietts Andreas Ruff <Riettsruff@gmail.com>
2020-11-18 03:08:57 +0700

    Update API Service

--- src/main/resources/static/js/api.service.js
+++ src/main/resources/static/js/api.service.js
@@ -1,58 +1,40 @@
 const API_URI = "http://116.254.101.228:8080/ma_test";
 
-async function AXIOS_POST(baseURL, data) {
-  try {
-    const request = await axios({
-      method: "POST",
-      baseURL,
-      data
-    });
-  
-    const response = request.data;
-    return response;
-  } catch (e) {
-    return false;
-  }
-}
+async function AXIOS_CUSTOM(method, baseURL, data = null) {
+  const request = await axios({
+    method,
+    baseURL,
+    data
+  });
 
-async function AXIOS_GET(baseURL) {
-  try {
-    const request = await axios({
-      method: "GET",
-      baseURL
-    });
-
-    const response = request.data;
-    return response;
-  } catch (e) {
-    return false;
-  }
+  const response = request.data;
+  return response;
 }
 
 function saveBasicInformation(data) {
-  return AXIOS_POST(`${API_URI}/profile/basic`, data);
+  return AXIOS_CUSTOM("POST", `${API_URI}/profile/basic`, data);
 }
 
 function saveCurrentOccupation(data) {
-  return AXIOS_POST(`${API_URI}/profile/currentoccupation`, data);
+  return AXIOS_CUSTOM("POST", `${API_URI}/profile/currentoccupation`, data);
 }
 
 function saveEducation(data) {
-  return AXIOS_POST(`${API_URI}/profile/education`, data);
+  return AXIOS_CUSTOM("POST", `${API_URI}/profile/education`, data);
 }
 
 function saveAddress(data) {
-  return AXIOS_POST(`${API_URI}/profile/address`, data);
+  return AXIOS_CUSTOM("POST", `${API_URI}/profile/address`, data);
 }
 
 function saveContact(data) {
-  return AXIOS_POST(`${API_URI}/profile/contact`, data);
+  return AXIOS_CUSTOM("POST", `${API_URI}/profile/contact`, data);
 }
 
 function saveUserRegistration(data) {
-  return AXIOS_POST(`${API_URI}/register`, data);
+  return AXIOS_CUSTOM("POST", `${API_URI}/register`, data);
 }
 
 function forgotPassword(email) {
-  return AXIOS_GET(`${API_URI}/forgotpassword/${email}`);
+  return AXIOS_CUSTOM("GET", `${API_URI}/forgotpassword/${email}`);
 }
