1c2fea9cc3b2ef1986729981e001c471b50ac60e
Rietts Andreas Ruff <Riettsruff@gmail.com>
2020-11-18 04:21:34 +0700

    Add More Entities

---
+++ src/main/java/com/kapitaselekta/tts/entities/Contact.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kapitaselekta.tts.entities;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Riett
+ */
+@Data
+public class Contact {
+    String id, phone, facebook, instagram, linkedin;
+}
