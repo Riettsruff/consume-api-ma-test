f898578473a772726aa273c4a39dc170832aaeb4
Rietts Andreas Ruff <Riettsruff@gmail.com>
2020-11-18 14:09:54 +0700

    Implement Spring Security With InMemory Auth

--- src/main/java/com/kapitaselekta/tts/configs/WebSecurityConfig.java
+++ src/main/java/com/kapitaselekta/tts/configs/WebSecurityConfig.java
@@ -5,10 +5,17 @@
  */
 package com.kapitaselekta.tts.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  *
@@ -17,6 +24,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+     
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User
+            .withUsername("riettsruff@gmail.com")
+            .password("$2y$10$12MToISuFdATL9zD2nw5YuF4Mm5vvJTp4NmlxlOkK0939ntsR6dcK")
+            .roles("LEARNER")
+            .build();    
+         
+        return new InMemoryUserDetailsManager(user);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,16 +53,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     "/js/**", 
                     "/img/**", 
                     "/register", 
-                    "/forgotpassword/**",
-                    "/"
+                    "/forgotpassword/**"
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
                 .loginPage("/login")
+                .usernameParameter("email")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
             .logout()
                 .permitAll();
     }
+   
 }
